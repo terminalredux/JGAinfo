bf21f14bc76275ef935c12e898bbd97d0ea06309
TerminalRedux <robertczaja20@gmail.com>
2017-10-21 17:49:21 +0200

    Delete api_keys.php

--- config/api_keys.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Open Weather Map
- */
-define('OPEN_WEATHER_MAP_API_KEY', 'b6d6f088d723e5446b3d5c5470e2ad9b');
-define('OPEN_WEATHER_MAP_JG_ID', 3097257);
-define('OPEN_WEATHER_MAP_URL', 'http://api.openweathermap.org/data/2.5/weather?id=%d&APPID=%s');
