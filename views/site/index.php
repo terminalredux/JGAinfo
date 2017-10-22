a3311810bb40912d358e0a586cb497f4e8deacfc
Unknown <robertczaja20@gmail.com>
2017-10-22 15:10:50 +0200

    Weather prototype in site/index

--- views/site/index.php
+++ views/site/index.php
@@ -8,14 +8,66 @@ $weather = new OpenWeatherMap();
   </div>
   <div class="col-md-6">
     <h2>Pogoda</h2>
-    <i class="fa fa-thermometer-three-quarters" aria-hidden="true"></i> Temperatura: <?= $weather->temp() ?> <br>
-    <i class="fa fa-tint" aria-hidden="true"></i> Wilgotność powietrza: <?= $weather->humidity() ?> <br>
-    <i class="fa fa-sun-o" aria-hidden="true"></i> Wschód: <?= $weather->sunrise() ?> <br>
-    <i class="fa fa-star" aria-hidden="true"></i> Zachód: <?= $weather->sunset() ?> <br>
-    <i class="fa fa-star" aria-hidden="true"></i> Ciśnienie: <?= $weather->pressure() ?> <br>
-    <i class="fa fa-cloud" aria-hidden="true"></i> Zachmurzenie: <?= $weather->clouds() ?> <br>
-    <h1>Wind speed <?= $weather->windSpeed() ?></h1>
-    <img src="<?= $weather->weatherImg() ?>" alt="pogoda" title="pogoda" class="weather-img">
+    <br><br>
+    <div class="row">
+        <div class="col-md-6">
+          <div class="weather-group">
+            <img src="<?= $weather->weatherImg() ?>" alt="pogoda" title="pogoda" class="weather-img">
+            <p>Zachmurzenie: <span><?= $weather->clouds() ?></span></p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="weather-group">
+            <img src="<?=  URL . 'web/img/044-temperature-2.png' ?>" alt="pogoda" title="pogoda" class="weather-img">
+            <p>Temperatura: <span><?= $weather->temp() ?></span></p>
+          </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+          <div class="weather-group">
+            <img src="<?=  URL . 'web/img/024-windy-1.png' ?>" alt="pogoda" title="pogoda" class="weather-img">
+            <p>Prędkość wiatru: <span><?= $weather->windSpeed() ?></span></p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="weather-group">
+            <img src="<?=  URL . 'web/img/002-drop.png' ?>" alt="pogoda" title="pogoda" class="weather-img">
+            <p>Wilgotność powietrza: <span><?= $weather->humidity() ?></span></p>
+          </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="weather-group">
+          <p>Ciśnienie: <span><?= $weather->pressure() ?></span></p>
+        </div>
+      </div>
+      <div class="col-md-6">
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+          <div class="weather-group">
+            <img src="<?=  URL . 'web/img/033-sunrise.png' ?>" alt="pogoda" title="pogoda" class="weather-img">
+            <p>Wschód: <span><?= $weather->sunrise() ?></span></p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="weather-group">
+            <img src="<?=  URL . 'web/img/034-moon.png' ?>" alt="pogoda" title="pogoda" class="weather-img">
+            <p>Zachód: <span><?= $weather->sunset() ?></span></p>
+          </div>
+        </div>
+    </div>
+
+
+
+
+
 
   </div>
 </div>
